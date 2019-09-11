7cd0e716d06a727cf411cb66cc9de7a9affa6d2b
SotoArmando <armando29@live.com>
2019-09-11 13:34:07 -0400

    feature(js_modules):fixed no needed functionality render

--- index.js
+++ index.js
@@ -1,44 +1,47 @@
 const player = (name, sign) => {
-    const sign = sign;
+    
     return { name, sign }
 }
 
 const gameBoard = function () {
     const cells = document.querySelectorAll(".cell");
-    const display = displayController();
-    const playerturn = 0;
+    const display = displayController(cells);
+    let playerturn = 0;
     const players = [player("player one", "O"), player("player two", "X")];
     let boardValuesxIsFilled = [false, false, false, false, false, false, false, false, false];
+
     const match = (cellindex) => {
+        console.log("am matching")
         if (!boardValuesxIsFilled[cellindex]) {
             let player = players[playerturn];
 
             cells[cellindex].innerHTML = player.sign;
 
-            display.render();
+            
             boardValuesxIsFilled[cellindex] = true;
             playerturn += 1;
             playerturn %= 2;
         }
     }
-    return {match}
+
+    cells.forEach((item,index) => item.addEventListener("click",() => match(index) ))
+    return {cells,match,display}
 };
 
 const displayController = (cells) => {
-    const cells = cells;
-    const htmlcontainer = Document.querySelector("main")
-    const render = (contextboardValues) => {
-        contextboardValues.map((value, index) => cells[index].innerHTML = value);
-    }
+    const htmlcontainer = document.querySelector("main")
+
     const display = (contextmsg) => {
         const msg = `<label>${contextmsg}</label>`;
         htmlcontainer += msg;
     }
-    return { render, display }
+    return {  display }
 };
 
+
 const Game = gameBoard();
-Game.render();
+
+
 
 
 
