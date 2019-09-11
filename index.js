4205dfb4cd9b646d9d04b683c161f58cd2146d2c
SotoArmando <armando29@live.com>
2019-09-11 13:11:08 -0400

    feature(js_modules): Added gameBoard match DisplayController Display functions

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
-const gameBoard = function() {};
+const player = (name, sign) => {
+    const sign = sign;
+    return { name, sign }
+}
 
-const displayController = function() {};
+const gameBoard = function () {
+    const cells = document.querySelectorAll(".cell");
+    const display = displayController();
+    const playerturn = 0;
+    const players = [player("player one", "O"), player("player two", "X")];
+    let boardValuesxIsFilled = [false, false, false, false, false, false, false, false, false];
+    const match = (cellindex) => {
+        if (!boardValuesxIsFilled[cellindex]) {
+            let player = players[playerturn];
 
-const cells = document.querySelectorAll(".cell");
-let boardValues = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const fillCells = ({value}, index) => value = boardValues[index];
-cells.forEach(fillCells)
+            cells[cellindex].innerHTML = player.sign;
+
+            display.render();
+            boardValuesxIsFilled[cellindex] = true;
+            playerturn += 1;
+            playerturn %= 2;
+        }
+    }
+    return {match}
+};
+
+const displayController = (cells) => {
+    const cells = cells;
+    const htmlcontainer = Document.querySelector("main")
+    const render = (contextboardValues) => {
+        contextboardValues.map((value, index) => cells[index].innerHTML = value);
+    }
+    const display = (contextmsg) => {
+        const msg = `<label>${contextmsg}</label>`;
+        htmlcontainer += msg;
+    }
+    return { render, display }
+};
+
+const Game = gameBoard();
+Game.render();
+
+
+
