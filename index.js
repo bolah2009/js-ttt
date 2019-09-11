cb18a004abd74e43b2fc9d1e3e06452f80e6abfd
Buari Bola Ahmed <bola_ahmed2002@yahoo.com>
2019-09-11 23:15:19 +0100

    refactor code to pass linter errors/warnings

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const player = (name, sign) => {
-  return { name, sign };
+const player = (name, sign) => ({ name, sign });
+
+
+const displayController = () => {
+  const htmlcontainer = document.querySelector('div.info');
+
+  const sendmsg = (contextmsg) => {
+    htmlcontainer.textContent = contextmsg;
+  };
+  return { sendmsg };
 };
 
-const gameBoard = function() {
-  const cells = document.querySelectorAll(".cell");
+const gameBoard = () => {
+  const cells = document.querySelectorAll('.cell');
   const display = displayController(cells);
   let started = false;
   let players = [];
-  const isWinner = cells => {
+  const isWinner = (cellsValue) => {
     const winPos = [
       [0, 1, 2],
       [3, 4, 5],
@@ -16,15 +24,15 @@ const gameBoard = function() {
       [1, 4, 7],
       [2, 5, 8],
       [0, 4, 8],
-      [2, 4, 6]
+      [2, 4, 6],
     ];
     let win = false;
 
-    winPos.forEach(pos => {
+    winPos.forEach((pos) => {
       if (
-        cells[pos[0]] &&
-        cells[pos[0]] === cells[pos[1]] &&
-        cells[pos[0]] === cells[pos[2]]
+        cellsValue[pos[0]]
+        && cellsValue[pos[0]] === cellsValue[pos[1]]
+        && cellsValue[pos[0]] === cellsValue[pos[2]]
       ) {
         win = true;
       }
@@ -34,53 +42,39 @@ const gameBoard = function() {
 
   let playerturn = 0;
 
-  let boardValuesxIsFilled = [
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false
-  ];
+  let boardValuesxIsFilled = [];
 
-  const match = cellindex => {
-    debugger;
+  const match = (cellindex) => {
     if (!boardValuesxIsFilled[cellindex] && started) {
-      let player = players[playerturn];
-      cells[cellindex].innerHTML = player.sign;
-      boardValuesxIsFilled[cellindex] = player.sign;
+      const currentPlayer = players[playerturn];
+      cells[cellindex].textContent = currentPlayer.sign;
+      boardValuesxIsFilled[cellindex] = currentPlayer.sign;
       if (isWinner(boardValuesxIsFilled)) {
-        display.sendmsg(`the is a winner :D. congrats !! ${player.name}`);
-      } else if ([...cells].every(item => item.innerHTML != "_")) {
-        display.sendmsg(`all cells are filled, game finished.`);
+        display.sendmsg(`the is a winner :D. congrats !! ${currentPlayer.name}`);
+      } else if ([...cells].every((item) => item.textContent !== '_')) {
+        display.sendmsg('all cells are filled, game finished.');
       }
       playerturn += 1;
       playerturn %= 2;
     }
   };
 
-  cells.forEach((item, index) =>
-    item.addEventListener("click", () => match(index))
-  );
+  cells.forEach((item, index) => item.addEventListener('click', () => match(index)));
 
   const start = () => {
-    debugger;
     if (!started) {
       if (
-        document.querySelector("#playerone-name").value != "" &&
-        document.querySelector("#playertwo-name").value != ""
+        document.querySelector('#playerone-name').value !== ''
+        && document.querySelector('#playertwo-name').value !== ''
       ) {
         players = [
-          player(document.querySelector("#playerone-name").value, "O"),
-          player(document.querySelector("#playertwo-name").value, "X")
+          player(document.querySelector('#playerone-name').value, 'O'),
+          player(document.querySelector('#playertwo-name').value, 'X'),
         ];
         started = true;
-        display.sendmsg("there ya go :)");
+        display.sendmsg('there ya go :)');
       } else {
-        display.sendmsg("name should not be empty.");
+        display.sendmsg('name should not be empty.');
       }
     } else {
       started = !started;
@@ -89,29 +83,29 @@ const gameBoard = function() {
   const reset = () => {
     started = false;
     players = [];
-    boardValuesxIsFilled = [
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false
-    ];
-    cells.forEach(item => (item.innerHTML = "_"));
+    boardValuesxIsFilled = [];
+    cells.forEach((item, index) => {
+      cells[index].textContent = '_';
+    });
   };
-  return { cells, match, display, start, reset };
-};
-
-const displayController = cells => {
-  const htmlcontainer = document.querySelector("div.info");
-
-  const sendmsg = contextmsg => {
-    htmlcontainer.innerHTML = contextmsg;
+  return {
+    cells, match, display, start, reset,
   };
-  return { sendmsg };
 };
 
 const Game = gameBoard();
+
+const handlers = ({ target: { value } }) => {
+  switch (value) {
+    case 'start':
+      Game.start();
+      break;
+    case 'reset':
+      Game.reset();
+      break;
+    default:
+      break;
+  }
+};
+
+document.addEventListener('click', handlers);
