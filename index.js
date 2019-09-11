90600efb76511fe6b5581964bf256eda7841c4b0
Buari Bola Ahmed <bola_ahmed2002@yahoo.com>
2019-09-11 19:33:53 +0100

    add winning logic

--- index.js
+++ index.js
@@ -1,47 +1,74 @@
 const player = (name, sign) => {
-    
-    return { name, sign }
-}
-
-const gameBoard = function () {
-    const cells = document.querySelectorAll(".cell");
-    const display = displayController(cells);
-    let playerturn = 0;
-    const players = [player("player one", "O"), player("player two", "X")];
-    let boardValuesxIsFilled = [false, false, false, false, false, false, false, false, false];
-
-    const match = (cellindex) => {
-        console.log("am matching")
-        if (!boardValuesxIsFilled[cellindex]) {
-            let player = players[playerturn];
-
-            cells[cellindex].innerHTML = player.sign;
-
-            
-            boardValuesxIsFilled[cellindex] = true;
-            playerturn += 1;
-            playerturn %= 2;
-        }
-    }
-
-    cells.forEach((item,index) => item.addEventListener("click",() => match(index) ))
-    return {cells,match,display}
+  return { name, sign };
 };
 
-const displayController = (cells) => {
-    const htmlcontainer = document.querySelector("main")
+const gameBoard = function() {
+  const cells = document.querySelectorAll(".cell");
+  const display = displayController(cells);
+  const isWinner = cells => {
+    const winPos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    let win = false;
 
-    const display = (contextmsg) => {
-        const msg = `<label>${contextmsg}</label>`;
-        htmlcontainer += msg;
+    winPos.forEach(pos => {
+      if (cells[pos[0]] && cells[pos[0]] === cells[pos[1]] && cells[pos[0]] === cells[pos[2]]) {
+        win = true;
+      }
+    });
+    return win;
+  };
+
+  let playerturn = 0;
+  const players = [player("player one", "O"), player("player two", "X")];
+  let boardValuesxIsFilled = [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false
+  ];
+
+  const match = cellindex => {
+    if (!boardValuesxIsFilled[cellindex]) {
+      let player = players[playerturn];
+
+      cells[cellindex].innerHTML = player.sign;
+
+      boardValuesxIsFilled[cellindex] = player.sign;
+      console.log(boardValuesxIsFilled);
+      console.log(isWinner(boardValuesxIsFilled));
+
+      playerturn += 1;
+      playerturn %= 2;
     }
-    return {  display }
+  };
+
+  cells.forEach((item, index) =>
+    item.addEventListener("click", () => match(index))
+  );
+  return { cells, match, display };
 };
 
+const displayController = cells => {
+  const htmlcontainer = document.querySelector("main");
+
+  const display = contextmsg => {
+    const msg = `<label>${contextmsg}</label>`;
+    htmlcontainer += msg;
+  };
+  return { display };
+};
 
 const Game = gameBoard();
-
-
-
-
-
